5b132d9869c8031f43de3c43a4e3507ac8be0bb7
Leo-Xee <jang.xmin@gmail.com>
2022-05-17 21:53:01 +0900

    fix: Title 컴포넌트 테스트 수정

--- src/components/Title/Title.test.tsx
+++ src/components/Title/Title.test.tsx
@@ -4,12 +4,14 @@ import Title from "./Title";
 const title = "GitHub Finder";
 const desc = "Github Repository를 검색하고 Issues를 확인할 수 있습니다.";
 
-it("타이틀과 소개글을 보여준다.", () => {
-  render(<Title title={title} description={desc} />);
+describe("<Title />", () => {
+  it("타이틀과 소개글을 보여준다.", () => {
+    render(<Title title={title} description={desc} />);
 
-  const heading = screen.getByRole("heading", { name: "GitHub Finder" });
-  const description = screen.getByText("Github Repository를 검색하고 Issues를 확인할 수 있습니다.");
+    const heading = screen.getByRole("heading", { name: title });
+    const description = screen.getByText(desc);
 
-  expect(heading).toBeInTheDocument();
-  expect(description).toBeInTheDocument();
+    expect(heading).toBeInTheDocument();
+    expect(description).toBeInTheDocument();
+  });
 });
