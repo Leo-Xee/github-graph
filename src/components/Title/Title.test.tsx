9a936889c73ddf7dff63da032188f0695f1672d0
Jangmin Lee <jang.xmin@gmail.com>
2022-05-17 15:33:36 +0900

    refactor: Title 컴포넌트와 스타일 분리

--- src/components/Title/Title.test.tsx
+++ src/components/Title/Title.test.tsx
@@ -2,10 +2,14 @@ import { render, screen } from "@testing-library/react";
 import Title from "./Title";
 
 const title = "GitHub Finder";
+const desc = "Github Repository를 검색하고 Issues를 확인할 수 있습니다.";
 
-it("타이틀을 보여준다.", () => {
-  render(<Title title={title} />);
+it("타이틀과 소개글을 보여준다.", () => {
+  render(<Title title={title} description={desc} />);
 
   const heading = screen.getByRole("heading", { name: "GitHub Finder" });
+  const description = screen.getByText("Github Repository를 검색하고 Issues를 확인할 수 있습니다.");
+
   expect(heading).toBeInTheDocument();
+  expect(description).toBeInTheDocument();
 });
--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -1,52 +1,15 @@
-import { keyframes } from "@emotion/react";
-import styled from "@emotion/styled";
+import * as S from "./style";
 
 type TitleProps = {
   title: string;
+  description: string;
 };
 
-const move = keyframes`
- 0% {
-   background-position: 0%;
- } 
- 100% {
-   background-position: 100%;
- } 
-`;
-
-const Heading = styled.h1`
-  margin: 5rem auto 2rem;
-  width: fit-content;
-  padding: 0 1rem;
-  text-align: center;
-  line-height: 1;
-  font-size: 7rem;
-  font-weight: 900;
-  letter-spacing: -6px;
-  color: transparent;
-  background-size: 400%;
-  background-position: -100%;
-  background-image: linear-gradient(to left, #6741d9 0%, #fa5252 50%, #3b5bdb 100%);
-  background-clip: text;
-  -webkit-background-clip: text;
-  user-select: none;
-  animation: ${move} 5s infinite alternate-reverse;
-`;
-
-const Description = styled.p`
-  padding: 0 2rem;
-  letter-spacing: -1px;
-  text-align: center;
-  font-size: 2rem;
-  font-weight: 700;
-  user-select: none;
-`;
-
-function Title({ title }: TitleProps) {
+function Title({ title, description }: TitleProps) {
   return (
     <header>
-      <Heading>{title}</Heading>
-      <Description>Github Repository를 검색할 수 있고 Issues를 확인할 수 있습니다.</Description>
+      <S.Heading>{title}</S.Heading>
+      <S.Description>{description}</S.Description>
     </header>
   );
 }
