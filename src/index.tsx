68cab8ad30a32e00cb17d27d64cd42c925de4d24
Leo-Xee <jang.xmin@gmail.com>
2022-07-13 23:49:54 +0900

    fix: index.tsx를 React v18에 적합하도록 변경

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,15 @@
 import React from "react";
-import ReactDom from "react-dom";
+import ReactDOM from "react-dom/client";
+import { ApolloProvider } from "@apollo/client";
+import client from "./apollo";
 import App from "./App";
 
-ReactDom.render(<App />, document.getElementById("root"));
+const element = document.getElementById("root");
+if (!element) throw Error("Root container missing in index.html");
+
+const root = ReactDOM.createRoot(element);
+root.render(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+);
